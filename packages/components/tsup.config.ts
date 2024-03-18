7f981f8a5eb675a24f29895eb4c3235f9751211b
Syakir Rahman <syakirurahman@gmail.com>
2024-03-18 17:29:43 +0700

    publish test

--- packages/components/tsup.config.ts
+++ packages/components/tsup.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "tsup"
+import { sassPlugin } from 'esbuild-sass-plugin';
 
 export default defineConfig({
+  esbuildPlugins: [sassPlugin()],
   format: ["cjs", "esm"],
   target: "es2019",
   sourcemap: false,
