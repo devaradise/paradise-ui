b088e6120b9a8a281c8ea0ca5e8a29bacb0ed1bc
Syakir Rahman <syakirurahman@gmail.com>
2024-03-18 12:25:29 +0700

    feat(text-field) added more stories for text field component

--- packages/components/TextField/stories/TextField.stories.tsx
+++ packages/components/TextField/stories/TextField.stories.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { TextField, TextFieldProps } from '../src';
+import { LockKey, User, Eye, WarningCircle } from '@phosphor-icons/react'
 
 const meta = {
   title: 'Component/Form/TextField',
@@ -71,12 +72,12 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Basic: Story = (args: TextFieldProps) => {
+export const Usage: Story = (args: TextFieldProps) => {
   return (
-      <TextField {...args}/>
+    <TextField {...args}/>
   )
 }
-Basic.args = {
+Usage.args = {
   label: 'Main label',
   secondaryLabel: 'Secondary label',
   placeholder: 'Placeholder',
@@ -89,4 +90,111 @@ Basic.args = {
   suffix: 'Suffix',
   helperText: 'A note, description or instruction for this text field',
   errorMessage: '',
+}
+
+export const Basic: Story = {
+  parameters: {
+    controls: {
+      disable: true
+    }
+  },
+  render: () => (<>
+    <TextField placeholder="Placeholder"/>
+  </>
+  )
+}
+
+export const Controlled: Story = () => {
+  const [value, setValue] = useState<string>('')
+  return (<>
+    <TextField placeholder="Placeholder" value={value} onChange={(value) => setValue(value)}/>
+    <br/>
+    Value: { value }
+  </>
+  )
+}
+Controlled.parameters = {
+  controls: {
+    disable: true
+  }
+}
+
+export const Variants: Story = {
+  parameters: {
+    controls: {
+      disable: true
+    }
+  },
+  render: () => (<>
+    <TextField variant="box" placeholder='Placeholder'/>
+    <br/>
+    <TextField variant="line" placeholder='Placeholder'/>
+    <br/>
+    <TextField variant="filled" placeholder='Placeholder'/>
+  </>
+  )
+}
+
+export const Sizes: Story = {
+  parameters: {
+    controls: {
+      disable: true
+    }
+  },
+  render: () => (<>
+    <TextField size="sm" placeholder='sm: small'/>
+    <TextField size="md" placeholder='md: medium'/>
+    <TextField size="lg" placeholder='lg: large'/>
+  </>
+  )
+}
+
+export const States: Story = {
+  parameters: {
+    controls: {
+      disable: true
+    }
+  },
+  render: () => (<>
+    <TextField readOnly defaultValue='readOnly'/>
+    <TextField invalid defaultValue='invalid'/>
+    <TextField disabled defaultValue='disabled'/>
+  </>
+  )
+}
+
+export const LabelsAndMessages: Story = {
+  parameters: {
+    controls: {
+      disable: true
+    }
+  },
+  render: () => (<>
+    <TextField label="Text field" placeholder='Placeholder' secondaryLabel="required"/>
+    <br/>
+    <TextField label="Text field" placeholder='Placeholder' secondaryLabel={<div style={{color: 'red'}}>required</div>} helperText="A note, description or instruction for this text field" />
+    <br/>
+    <TextField label="Text field" placeholder='Placeholder' secondaryLabel={<WarningCircle/>} helperText="A note, description or instruction for this text field'"  errorMessage="An error message will override helper text"/>
+  </>
+  )
+}
+
+export const PrefixAndSuffix: Story = {
+  parameters: {
+    controls: {
+      disable: true
+    }
+  },
+  render: () => (<>
+    <TextField label="Text field" placeholder='Placeholder' prefix="Prefix"/>
+    <br/>
+    <TextField label="Text field" placeholder='Placeholder' prefix={<User />}/>
+    <br/>
+    <TextField label="Text field" placeholder='Placeholder' suffix="Suffix"/>
+    <br/>
+    <TextField label="Text field" placeholder='Placeholder' suffix={<Eye />}/>
+    <br/>
+    <TextField label="Text field" placeholder='Placeholder' prefix={<LockKey />} suffix={<Eye />}/>
+  </>
+  )
 }
