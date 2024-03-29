e66234684da9fbc2a91c0daef91a1b572933b132
Syakir Rahman <syakirurahman@gmail.com>
2024-03-29 14:03:17 +0700

    updated plop component generator file

--- plopfile.js
+++ plopfile.js
@@ -1,65 +1,65 @@
 const capitalize = (str) => {
-  return str.charAt(0).toUpperCase() + str.slice(1)
-}
+	return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
 const kebabCase = (str) => {
-  return str.replace(/([a-z0–9])([A-Z])/g, "$1-$2").toLowerCase();
-}
+	return str.replace(/([a-z0–9])([A-Z])/g, '$1-$2').toLowerCase();
+};
 
 const camelCase = (str) => {
-  return str.replace(/[-_](\w)/g, (_, c) => c.toUpperCase())
-}
+	return str.replace(/[-_](\w)/g, (_, c) => c.toUpperCase());
+};
 
 // const workspaces = ["components", "utils", "core"]
 
 /**
  * @param {import("plop").NodePlopAPI} plop
-*/
+ */
 module.exports = function main(plop) {
-  plop.setHelper("capitalize", (text) => {
-    return capitalize(camelCase(text))
-  })
-  plop.setHelper("kebabCase", (text) => {
-    return kebabCase(text)
-  })
+	plop.setHelper('capitalize', (text) => {
+		return capitalize(camelCase(text));
+	});
+	plop.setHelper('kebabCase', (text) => {
+		return kebabCase(text);
+	});
 
-  plop.setGenerator("component", {
-    description: "Generates a component package",
-    prompts: [
-      {
-        type: "input",
-        name: "componentName",
-        message: "Enter component name (eg. ComponentName): ",
-      },
-      {
-        type: "list",
-        name: "category",
-        message: "Enter component category: ",
-        choices: ["Form", "Panel", "Data", "Overlay", "Message", "Menu", "Media", "Misc"]
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "The description of this component:",
-      }
-    ],
-    actions(answers) {
-      const actions = []
+	plop.setGenerator('component', {
+		description: 'Generates a component package',
+		prompts: [
+			{
+				type: 'input',
+				name: 'componentName',
+				message: 'Enter component name (eg. ComponentName): '
+			},
+			{
+				type: 'list',
+				name: 'category',
+				message: 'Enter component category: ',
+				choices: ['Form', 'Panel', 'Data', 'Overlay', 'Feedback', 'Menu', 'Media', 'Misc']
+			},
+			{
+				type: 'input',
+				name: 'description',
+				message: 'The description of this component:'
+			}
+		],
+		actions(answers) {
+			const actions = [];
 
-      if (!answers) return actions
+			if (!answers) return actions;
 
-      const { componentName, category, description } = answers
+			const { componentName, category, description } = answers;
 
-      actions.push({
-        type: "addMany",
-        templateFiles: "plop/component/**",
-        destination: `./packages/components/{{capitalize componentName}}`,
-        base: "plop/component",
-        data: { componentName, category, description },
-        abortOnFail: true,
-      })
+			actions.push({
+				type: 'addMany',
+				templateFiles: 'plop/component/**',
+				destination: `./packages/components/{{capitalize componentName}}`,
+				base: 'plop/component',
+				data: { componentName, category, description },
+				abortOnFail: true
+			});
 
-      return actions
-    },
-  })
-}
+			return actions;
+		}
+	});
+};
