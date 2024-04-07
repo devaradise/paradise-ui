735ac9cea3b09cf7f15a4f7bbe8063f2c75bf2cb
Syakir Rahman <syakirurahman@gmail.com>
2024-04-08 01:16:09 +0700

    [TextField] restrict type prop only to certain values

--- packages/components/TextField/src/type.ts
+++ packages/components/TextField/src/type.ts
@@ -2,14 +2,16 @@ import { InputHTMLAttributes, ReactNode } from 'react';
 import { ComponentSize } from '@paradise-ui/common';
 
 export type TextFieldVariant = 'outlined' | 'line' | 'filled';
+export type TextFieldType = 'text' | 'number' | 'email' | 'url' | 'password' | 'search' | 'tel';
 
-export interface TextFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'prefix' | 'size'> {
+export interface TextFieldProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange' | 'prefix' | 'size' | 'type'> {
 	label?: ReactNode;
 	secondaryLabel?: ReactNode;
 	variant?: TextFieldVariant;
 	size?: ComponentSize;
 	name?: string;
 	className?: string;
+	type?: TextFieldType;
 	// overridingClass?: { [key:string]: string }
 	placeholder?: string;
 	invalid?: boolean;
