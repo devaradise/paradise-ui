8c56faac41a3900042d93bfb5bf2927513bcc0a0
Syakir Rahman <syakirurahman@gmail.com>
2024-04-08 01:31:24 +0700

    bumped package versions

--- packages/react/stories/ParadiseUIProvider.stories.tsx
+++ packages/react/stories/ParadiseUIProvider.stories.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect } from 'react';
+import { ChangeEvent } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Alert, Button, ParadiseUIProvider, TextField, useThemeMode } from '../src';
 import { ThemeModeControl, defaultColors } from '@paradise-ui/common';
